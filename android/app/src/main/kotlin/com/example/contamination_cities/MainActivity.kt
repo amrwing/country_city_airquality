980a2517867c4a5f05b721cbc7b2698d7d3291da
Antonio_Moreno <gamamurder@gmail.com>
2024-03-05 20:02:25 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/contamination_cities/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.contamination_cities
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/api/api_key.dart
@@ -0,0 +1,1 @@
+const String apiKey = "E7eXd+1R6qgj1rdsCf5MYg==PSjp9snG0eYpvyvz";
